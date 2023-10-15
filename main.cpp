1dff5218369d5f3f4a1223e4b6902ceb10578711
fayek2025 <fayekahmedrahat90@gmail.com>
2023-10-15 10:04:05 +0600

    sdl window

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-int main()
+using namespace  std;
+
+
+int main(int argc  , char* argv[])
 {
-    int a;
-    int b;
-    int c;
-    int d;
+        SDL_Window* window ;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << "SDL can not be initialized!" << SDL_GetError();
+
+    }
+    else
+    {
+        cout << "SDL video system is ready to go\n" << endl;
+
+    }
+
+    window = SDL_CreateWindow("Mini Football Game" , 
+                              0,
+                              0,
+                              640,
+                              480,
+                              SDL_WINDOW_SHOWN);
+
+    
+
+    //Delay our pause 
+    SDL_Delay(10000);
+
 }
