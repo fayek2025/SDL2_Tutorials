9986bf918ef14390b08c7e42664ab6f3d430e2ac
John Doe <fayekahmedrahat90@gmai.com>
2023-10-16 20:06:35 +0600

    comit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <glad/glad.h>
 
 //version of opengl
-
+//g++ main.cpp -o game glad/src/glad.c -I./glad/include -lSDL2 -ldl
 
 using namespace std;
 
@@ -12,8 +12,9 @@ using namespace std;
 
 int main(int argc  , char* argv[])
 {
-
+        SDL_Surface *screen;
         SDL_Window* window ;
+        SDL_Surface *image;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -26,29 +27,37 @@ int main(int argc  , char* argv[])
 
     }
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION , 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION , 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK , SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER , 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE , 24);
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION , 4);
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION , 4);
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK , SDL_GL_CONTEXT_PROFILE_CORE);
+    // SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER , 1);
+    // SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE , 24);
     window = SDL_CreateWindow("Mini Football Game" , 
                               50,
                               0,
                               640,
                               480,
-                              SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+                              SDL_WINDOW_SHOWN ); //| SDL_WINDOW_OPENGL
 
-    SDL_GLContext context;
-    context = SDL_GL_CreateContext(window);
+    // SDL_GLContext context;
+    // context = SDL_GL_CreateContext(window);
+
+    // //intead of renderer we are directly drawing to the screen
+     screen = SDL_GetWindowSurface(window);
+    image = SDL_LoadBMP("./images/sample_640×426.bmp");
+     SDL_BlitSurface(image, NULL, screen, NULL); // blit it to the screen
+     SDL_FreeSurface(image);
+     SDL_UpdateWindowSurface(window);
+
 
     //setup our function pointer
-    gladLoadGLLoader(SDL_GL_GetProcAddress);
+    // gladLoadGLLoader(SDL_GL_GetProcAddress);
     
     bool gameIsRunning = true;
 
     while(gameIsRunning != false)
     {
-        glViewport(0 , 0 , 640 , 480);
+        // glViewport(0 , 0 , 640 , 480);
 
         SDL_Event event;
 
@@ -68,10 +77,10 @@ int main(int argc  , char* argv[])
                 }
             }
 
-            glClearColor(1.0f , 0.0f , 0.0f , 1.0f);
+            // glClearColor(1.0f , 0.0f , 0.0f , 1.0f);
     
-            glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-            SDL_GL_SwapWindow(window);
+            //  glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+            //  SDL_GL_SwapWindow(window);
         }
     }
 
@@ -81,12 +90,11 @@ int main(int argc  , char* argv[])
     //Destroying windows
     SDL_DestroyWindow(window);
 
-<<<<<<< HEAD
+
     
-=======
     //Quiting SDL window also turning off audio or video system
     SDL_Quit();
->>>>>>> 14112205eace7cc0f27f2fe7956460758f4856cb
+
 
 
 }
