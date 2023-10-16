14112205eace7cc0f27f2fe7956460758f4856cb
fayek2025 <fayekahmedrahat90@gmail.com>
2023-10-16 13:51:02 +0600

    commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <glad/glad.h>
+
+//version of opengl
+
+
 using namespace std;
 
-using namespace  std;
+
 
 
 int main(int argc  , char* argv[])
 {
-<<<<<<< HEAD
+
         SDL_Window* window ;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -21,65 +26,63 @@ int main(int argc  , char* argv[])
 
     }
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION , 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION , 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK , SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER , 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE , 24);
     window = SDL_CreateWindow("Mini Football Game" , 
-                              0,
+                              50,
                               0,
                               640,
                               480,
-                              SDL_WINDOW_SHOWN);
+                              SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
+    SDL_GLContext context;
+    context = SDL_GL_CreateContext(window);
+
+    //setup our function pointer
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
     
+    bool gameIsRunning = true;
 
-    //Delay our pause 
-    SDL_Delay(10000);
-
-=======
-    SDL_Window  * window = nullptr;
-
-    if (SDL_Init(SDL_INIT_VIDEO) == 0)
+    while(gameIsRunning != false)
     {
-        cout << "No SDL input is valid!"<<endl;
+        glViewport(0 , 0 , 640 , 480);
 
-        
-    }
-    else{
-        cout << " Sdl run perfectly!"<< endl;
-        
-    }
-    
-    //Creating new windows
-    window = SDL_CreateWindow("my first game",0 , 0 , 700 , 300 ,
-     SDL_WINDOW_SHOWN);
-
-     bool gameIsRunning = true;
-
-     //Initiating infinite loop
-     while(gameIsRunning)
-     {
         SDL_Event event;
-        //starting our event loop
-        while (SDL_PollEvent(&event))
-        
-        {
-            //handle each specific event
-            if(event.type == SDL_QUIT)
+
+        while(SDL_PollEvent(&event))
+        {   
+            //handling events
+            if (event.type == SDL_QUIT)
             {
                 gameIsRunning = false;
             }
-        }
-        
-     }
+
+            if (event.type = SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_w)
+                {
+                    cout << "'W' key pressed!" << endl;
+                }
+            }
+
+            glClearColor(1.0f , 0.0f , 0.0f , 1.0f);
     
+            glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+            SDL_GL_SwapWindow(window);
+        }
+    }
 
-    SDL_Delay(3000);
-
+    //Delay our pause 
+    //SDL_Delay(3000);
+    
+    //Destroying windows
     SDL_DestroyWindow(window);
 
-    SDL_QUIT;
+    //Quiting SDL window also turning off audio or video system
+    SDL_Quit();
 
 
-        
-        
-    
->>>>>>> b087b7f8ae16aae230ecc7b60a8f3bd8474294ef
 }
